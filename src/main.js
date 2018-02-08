b514950a781c0b7f723ec2c89518f67c599afabd
Thanakorn Amnuaywiboolpol <l3irdWings@gmail.com>
2018-02-08 10:58:49 +0700

    Clean up for demo only

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
 import BootstrapVue from 'bootstrap-vue'
 import firebase from 'firebase'
 
@@ -24,7 +25,7 @@ Vue.prototype.$firebase = firebase.initializeApp(config)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router: router,
+  router,
   components: { App },
   template: '<App/>'
 })

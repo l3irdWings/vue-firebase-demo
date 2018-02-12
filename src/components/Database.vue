483240d0e297d669630c476b63c64ade80f8b69a
Thanakorn Amnuaywiboolpol <l3irdWings@gmail.com>
2018-02-12 16:35:12 +0700

    Complete storage

--- src/components/Database.vue
+++ src/components/Database.vue
@@ -3,16 +3,16 @@
     <h1><b>Database</b></h1>
     <b-container>
       <b-row>
-        <b-col id="input_col">
-          <div id="new_message_div">
+        <b-col id="left_col">
+          <div id="left_inside_div">
             <img src="./../assets/logo.png">
             <img src="./../assets/firebase.png">
-            <b-form-input id="new_message_input" type="text" v-model="newMessage" required placeholder="Write Comment"></b-form-input>
+            <b-form-input id="new_message_input" v-model="newMessage" required placeholder="Write Comment"></b-form-input>
             <b-button id="new_message_button" v-on:click="addMessage()">Send</b-button>
           </div>
         </b-col>
-        <b-col id="output_col">
-          <div id="messages_div">
+        <b-col id="right_col">
+          <div id="right_inside_div">
             <b-badge v-for="message in messages" v-bind:key="message.id"><h4>{{ message.message }}</h4></b-badge>
           </div>
         </b-col>
@@ -26,7 +26,6 @@ export default {
   name: 'database',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App',
       messages: [],
       newMessage: ''
     }
@@ -93,18 +92,6 @@ export default {
     margin-bottom: 10px;
     padding: 10px 10px 0;
   }
-  #input_col {
-    position: relative;
-    height: 500px;
-  }
-  #new_message_div {
-    position: absolute;
-    top: 50%;
-    left: 20px;
-    right: 20px;
-    height: 320px;
-    margin-top: -160px;
-  }
   #new_message_input {
     margin-top: 10px;
     height: 50px;
@@ -112,17 +99,5 @@ export default {
   #new_message_button {
     height: 50px;
     margin-top: 10px;
-  }
-  #output_col {
-    position: relative;
-    height: 500px;
-  }
-  #messages_div {
-    position: absolute;
-    top: 50%;
-    left: 20px;
-    right: 20px;
-    height: 320px;
-    margin-top: -160px;
   }
 </style>
--- src/components/Storage.vue
+++ src/components/Storage.vue
@@ -1,15 +1,96 @@
 <template>
   <div class="storage">
     <h1><b>Storage</b></h1>
+    <b-container>
+      <b-row>
+        <b-col id="left_col">
+          <div id="left_inside_div">
+            <img src="./../assets/logo.png">
+            <img src="./../assets/firebase.png">
+            <b-form-file id="new_image_input" v-model="newImage" :state="Boolean(newImage)" accept=".jpg, .png" placeholder="Choose a file..."></b-form-file>
+            <b-button id="new_image_button" v-on:click="uploadImage()">Upload</b-button>
+          </div>
+        </b-col>
+        <b-col id="right_col">
+          <div id="right_inside_div">
+            <b-img v-for="image in images" v-bind:key="image.id" v-bind:src="image.url" fluid alt="image" height="100"></b-img>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'storage'
+  name: 'storage',
+  data () {
+    return {
+      images: [],
+      newImage: null
+    }
+  },
+  created () {
+    let self = this
+    this.$firebase.database().ref('image').once('value').then(snapshot => {
+      snapshot.forEach(function (imageSnapshot) {
+        let image = imageSnapshot.val()
+        image.id = imageSnapshot.key
+        self.$firebase.storage().refFromURL(image.storage_location).getDownloadURL().then(url => {
+          image.url = url
+          self.images.push(image)
+          if (self.images.length === snapshot.numChildren()) {
+            observeImage()
+          }
+        })
+      })
+    })
+
+    function observeImage () {
+      self.$firebase.database().ref('image').on('child_added', snapshot => {
+        const index = self.images.findIndex(image => image.id === snapshot.key)
+        if (index === -1) {
+          let image = snapshot.val()
+          image.id = snapshot.key
+          self.$firebase.storage().refFromURL(image.storage_location).getDownloadURL().then(url => {
+            image.url = url
+            self.images.push(image)
+          })
+        }
+      })
+
+      self.$firebase.database().ref('image').on('child_removed', snapshot => {
+        const index = self.images.findIndex(image => image.id === snapshot.key)
+        if (index >= 0) {
+          let image = snapshot.val()
+          self.$firebase.storage().refFromURL(image.storage_location).delete().then(function () {
+            self.images.splice(index, 1)
+          })
+        }
+      })
+    }
+  },
+  methods: {
+    uploadImage () {
+      let self = this
+      this.$firebase.storage().ref(`/image/${this.newImage.name}`).put(this.newImage).then(function () {
+        // Upload completed successfully, now we can get the download URL
+        self.$firebase.database().ref('image').push({
+          storage_location: `gs://${self.$firebase.storage().ref().bucket}/image/${self.newImage.name}`
+        })
+      })
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+  #new_image_input {
+    margin-top: 10px;
+    height: 50px;
+  }
+  #new_image_button {
+    height: 50px;
+    margin-top: 10px;
+  }
 </style>
